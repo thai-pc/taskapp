78090906731adc0b79e59dcb7ab7c1b0bfb72850
thai-pc <vodongthais1@gmail.com>
2024-06-29 22:43:26 +0700

    cập nhật hôm nay

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,10 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
-
+use \App\Controllers\HomeController;
+use \App\Controllers\TaskController;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', [HomeController::class, 'index']);
+$routes->get('/tasks', [TaskController::class, 'index']);
